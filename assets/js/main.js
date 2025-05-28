1ccac7af769164a645c627f9d3042200392947d1
rahat1661 <19jesmin96@gmail.com>
2025-05-28 18:58:59 +0600

    update slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,39 @@
 (function($){
 
-    var swiper = new Swiper(".ip_hero_slider", {
+    var heroSlider = new Swiper(".ip_hero_slider", {
         effect: "cards",
         grabCursor: true,
         initialSlide: 1,
+      });
+    var seriesSlider = new Swiper(".ip_series_slider.slider_ltr", {
+        slidesPerView: "auto",
+        spaceBetween: 24,
+        speed : 3000,
+        loop : true,
+        autoplay:{
+          delay: 0,
+          enabled : true
+        }
+      });
+    var slider = new Swiper(".ip_series_slider.slider_rtl", {
+        slidesPerView: "auto",
+        spaceBetween: 24,
+        speed : 3000,
+        loop : true,
+        autoplay:{
+          delay: 0,
+          enabled : true
+        }
+      });
+    var brandsSlider = new Swiper(".ip_brands_slider", {
+        slidesPerView: "auto",
+        spaceBetween: 32,
+        speed : 3000,
+        loop : true,
+        autoplay:{
+          delay: 0,
+          enabled : true
+        }
       });
     
 
